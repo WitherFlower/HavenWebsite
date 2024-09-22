cc2600dfab356291dabd9871be772896e36a3634
WitherFlower <maxime.barniaudy@gmail.com>
2024-09-22 13:18:06 +0200

    tools

---
+++ src/routes/tools/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Converter from "./Converter.svelte";
+	import WordCounter from "./WordCounter.svelte";
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="row">
+	<Converter />
+	<WordCounter />
+</div>
+
+<style>
+	.row {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		justify-content: space-between;
+	}
+</style>
---
+++ src/routes/tools/Converter.svelte
@@ -0,0 +1,30 @@
+<script>
+	let inputString;
+</script>
+
+<div class="text-column tool">
+	<h1>Dec to Everything converter</h1>
+	<p>Input your number here :</p>
+	<input bind:value={inputString} />
+	<h2>HEX</h2>
+	<p>{parseInt(inputString).toString(16)}</p>
+	<h2>BIN</h2>
+	<p>{parseInt(inputString).toString(2)}</p>
+</div>
+
+<style>
+	.tool {
+		max-width: 40%;
+	}
+	h1 {
+		font-size: 3em;
+		font-weight: bold;
+	}
+	h2 {
+		font-size: 2em;
+		font-weight: bold;
+	}
+	input {
+		font-size: 1.5em;
+	}
+</style>
